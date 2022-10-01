c1f609d5fb52b1ec40a9efa369a1ef36bba39f90
temeddix <temeddix@gmail.com>
2022-10-01 16:18:28 +0900

    Make the game work

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    println!("The secret number is: {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    let mut guess: String;
+    let mut guess_number: u32;
 
-    println!("You guessed: {guess}");
+    loop {
+        guess = String::new();
+
+        println!("Please input your guess.");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        guess = guess.trim().to_string();
+
+        match guess.parse() {
+            Ok(number) => guess_number = number,
+            Err(_error) => {
+                guess = "0".to_string();
+                guess_number = 0;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
